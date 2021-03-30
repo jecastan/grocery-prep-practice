2f1ac391da097cd288d7385a2e9585384971983d
Jesse Castaneda Lutz <jesse.castaneda.lutz@gmail.com>
2021-03-29 23:43:20 -0700

    add mongo db and verified get, post requests

--- backend/index.js
+++ backend/index.js
@@ -1,35 +1,79 @@
 const express = require('express')
 const app = express()
 
+const mongoose = require('mongoose')
+
+const Recipe = require('./models/recipe')
+
+mongoose.connect("mongodb+srv://jecastan:10lng0lst0nm@cluster0.sam2k.mongodb.net/groceryPrepPractice?retryWrites=true&w=majority", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
+    useCreateIndex: true
+}).then(() => console.log('Connected to MongoDB'))
+
+const ribeyeRecipe = new Recipe({
+    name: "Cast Iron Ribeye",
+    desc:"Perfect Keto Dinner",
+    picture:"../imgs/Ribeye_Steak.jpg",
+    ratings: [5, 4, 3],
+    servings: 1,
+    ingredients: [
+        {ingredient: "ribeye, 1 lb each", amount: 1},
+        {ingredient: "tbsp salt", amount: 1},
+        {ingredient: "tsp pepper", amount: 1},
+        {ingredient: "tbsp canola oil", amount: 1}
+    ],
+    instructions: [
+        "Season ribeye with salt and pepper on both sides",
+        "Allow ribeye to sit for 30-60 minutes",
+        "Pre-heat oven to 350 deg",
+        "Heat cast iron with oil on high",
+        "Place ribeye on cast iron once oil starts to smoke",
+        "Sear for 2 minutes per side",
+        "Place cast iron in oven for 6 minutes",
+        "Remove from oven and allow to rest in pan for 10 minutes"
+    ]
+})
+
+ribeyeRecipe.save()
+    
 app.use(express.static('C:/Users/Jesse Castenada Lutz/grocery-prep-practice'))
 
 app.use(express.json())
 
-app.get('/api/recipe', (req, res) => {
-    res.status(200)
-    res.send('list of recipes requrested')
+const getRecipes = async () => {
+    return await Recipe.find({})
+}
+
+const getRecipe = async name => {
+    return await Recipe.find({name: name})
+}
+
+const postRating = async (name, rating) => {
+    return await Recipe.findOneAndUpdate(
+        { name: name },
+        { $push: {ratings: rating} }
+    )
+}
+
+app.get('/api/recipe', async (req, res) => {
+    const name = req.query.name
+
+    let recipes
+    if (name === undefined)
+        recipes = await getRecipes()
+    else
+        recipes = await getRecipe(name)
+    res.json(recipes)
 })
 
-app.get('/api/recipe/:name', (req, res) => {
-    const name = req.params.name
+app.post('/api/rating', async (req, res) => {
+    const name = req.body.name
+    const ratings = req.body.ratings
 
-    if (typeof name === undefined || name.length === 0) {
-        res.status(400)
-        res.send('error: no name :(')
-    }
-
-    res.status(200)
-    res.send(`instructions for ${name}`)
-})
-
-app.post('/api/rating', (req, res) => {
-    const rating = req.body.rating
-    const id = req.body.id
-
-    console.log(rating, id)
-
-    res.status(200)
-    res.send(`rating of ${rating} received for recipe ${id}`)
+    const rating = await postRating(name, ratings)
+    res.send()
 })
 
 app.listen(3000)
